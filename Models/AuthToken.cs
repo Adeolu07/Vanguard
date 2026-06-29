43cb242e6048dc2ac2fa9e116152d1989fff32bd
aisosa87 <johnfisherehondor@gmail.com>
2026-06-29 11:55:43 +0100

    Add AuthToken table and migration

---
+++ Models/AuthToken.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vanguard.Models;
+
+public class AuthToken
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Token { get; set; } = string.Empty;
+
+    public DateTime ExpiryDate { get; set; }
+}
